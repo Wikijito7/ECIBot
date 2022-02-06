0a254ca5c4fdf9e827681dd37564d17f97257d47
Wikijito7 <wikyfg@gmail.com>
2022-02-06 11:53:11 +0100

    Changes
    - Added tts command to create messages and reproduce it on the channel.
    - Added pool option to create a "yes/no" pool
    - Fixes.

--- utils.py
+++ utils.py
@@ -21,5 +21,19 @@ def get_sounds():
     return os.listdir(audio_path())
 
 
+def get_sounds_list():
+    sounds = get_sounds()
+    blank_space = "\u2800"
+    sounds.sort()
+    sounds = [sound.replace(".mp3", "") + blank_space * 2 for sound in sounds]
+    list_sounds = [[], [], []]
+
+    for x in range(len(sounds)):
+        sound = sounds[x]
+        pos = x % 3
+        list_sounds[pos].append(sound)
+
+    return list_sounds
+
 if __name__ == "__main__":
     pass

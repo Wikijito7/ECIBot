b29bd720eb6ebf0599f0e0b8db590c122ae6949e
Wikijito7 <wikyfg@gmail.com>
2022-03-26 19:13:17 +0100

    Version 0.7.5
    - Finished youtube support
    - Changed queue
    - Added multisound support
    - Changed sound system.

--- youtube.py
+++ youtube.py
@@ -1,25 +1,25 @@
 import youtube_dl
 import os
 
-baseUrl = "./yt/"
+yt_base_url = "./yt/"
 
 
 def check_base_dir():
-    if not os.path.exists(baseUrl):
-        os.makedirs(baseUrl)
+    if not os.path.exists(yt_base_url):
+        os.makedirs(yt_base_url)
 
 
 async def get_youtube_video(url, listener):
     check_base_dir()
     print(url)
     ydl_opts = {
-        'outtmpl':f'{baseUrl}%(title)s.%(ext)s',
+        'outtmpl':f'{yt_base_url}%(title)s.%(ext)s',
         'extractaudio': True,
         'format': 'worstaudio/140/17/36/22',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
-            'preferredquality': '64',
+            'preferredquality': '92',
         }],
         'progress_hooks': [listener],
     }

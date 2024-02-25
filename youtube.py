dd2f68c4389e8a2a1349d3099c6317b4faa89744
Javi <45560967+javdc@users.noreply.github.com>
2024-02-25 19:16:03 +0100

    Play yt-dlp audios without downloading them first
    Now audio loading is a lot quicker, and no size or length checks are required, because the audios are now loaded on the go! If the content found by yt-dlp is a list the contents of that list are iterated accordingly.
    
    Also made some refactoring and improvements:
    - Removed all download logic
    - Refactor add to queue logic to a new method to avoid repeating code
    - Now there are three sound types: file, tts and url
    - Print exceptions thrown when executing bot commands

--- youtube.py
+++ youtube.py
@@ -1,21 +1,6 @@
 import yt_dlp
-import os
 
-yt_base_url = "./yt/"
-MAX_VIDEO_DURATION = 86400 # in seconds
-MAX_VIDEO_SIZE = 800 * 1024 * 1024 # in bytes
 DOWNLOAD_FORMATS = '251/250/249/http-950/m4a/worst'
-
-def check_base_dir():
-    if not os.path.exists(yt_base_url):
-        os.makedirs(yt_base_url)
-
-
-def clear_yt():
-    check_base_dir()
-    for file in os.listdir(yt_base_url):
-        os.remove(os.path.join(yt_base_url, file))
-
 
 def extract_yt_dlp_info(url):
     try: 
@@ -47,15 +32,9 @@ def yt_search_and_extract_yt_dlp_info(search_query):
         return None
 
 
-def get_youtube_dlp_video(url, listener):
-    check_base_dir()
-    print(f"youtube: get_youtube_video >> {url}")
-    ydl_opts = {
-        'outtmpl':f'{yt_base_url}%(title)s.%(ext)s',
-        'extractaudio': True,
-        'format': DOWNLOAD_FORMATS,
-        'progress_hooks': [listener],
-    }
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+def is_suitable_for_yt_dlp(url):
+    extractors = yt_dlp.extractor.gen_extractors()
+    for e in extractors:
+        if e.suitable(url) and e.IE_NAME != 'generic':
+            return True
+    return False

504eccd59ceb71aa10ce4fba60b1aff064a1e643
Javi <45560967+javdc@users.noreply.github.com>
2024-05-01 18:35:23 +0200

    Several improvements in confetti command (#52)
    - Fix repeated songs in the mix: We were previously playing the entire
    YT Music channel of Confetti, so some songs that are the same but
    belonging to different albums would be added to the queue. Instead, use
    the YT Music playlist of songs, which shouldn't include repeated songs
    from different albums.
    
    - Actually load full list of Confetti songs before picking them instead
    of only loading 30.
    
    - Blacklist non-Confetti songs from the list. This is just YouTube Music
    fault.

--- youtube.py
+++ youtube.py
@@ -10,12 +10,12 @@ YT_DLP_EXTRACTORS = yt_dlp.extractor.gen_extractors()
 MAX_PLAYLIST_ITEMS = 30
 
 
-def extract_yt_dlp_info(url: str) -> Any:
+def extract_yt_dlp_info(url: str, retrieve_full_playlist: bool = False) -> Any:
     try: 
         ydl_opts = {
             'format': YT_DLP_FORMATS,
             'extract_flat': True,  # Don't try to obtain information from nested content (very slow in long playlists)
-            'playlistend': MAX_PLAYLIST_ITEMS + 1,  # Limit max playlists items to avoid excessive pagination and add one more to be able to check if list was too long and send message
+            'playlistend': None if retrieve_full_playlist else MAX_PLAYLIST_ITEMS + 1,  # Limit max playlists items by default to avoid excessive pagination and add one more to be able to check if list was too long and send message
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             return ydl.extract_info(url, download=False)

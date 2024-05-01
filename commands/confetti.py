9ffb5f3150607ae5ffaa3d3c66b7ebc87f36647e
Javi <45560967+javdc@users.noreply.github.com>
2024-05-01 18:10:44 +0200

    Several improvements in confetti command
    - Fix repeated songs in the mix: We were previously playing the entire YT Music channel of Confetti, so some songs that are the same but belonging to different albums would be added to the queue. Instead, use the YT Music playlist of songs, which shouldn't include repeated songs from different albums.
    
    - Actually load full list of Confetti songs before picking them instead of only loading 30.
    
    - Blacklist non-Confetti songs from the list. This is just YouTube Music fault.

--- commands/confetti.py
+++ commands/confetti.py
@@ -9,18 +9,27 @@ from commands.play import on_play
 from youtube import extract_yt_dlp_info
 
 
-async def on_confetti(number_of_songs: int, author_name: str, guild_id: int, voice_channel: VocalGuildChannel,
+# These songs are incorrectly tagged as Confetti songs in YT Music
+NOT_CONFETTI_SONGS = ['94ruVm4lK6s', '6k7rxay1MOc', 'MFU1vjivNAs', 'r9n_ssGCtl0', 'zneFDkhteTw',
+                      'MRnn1i-DH4Q', 'F_Y5meRFsts', 'ktdiMQPhBgE', '3Kvz6ExY8OU']
+
+
+async def on_confetti(number_of_songs: int, author_name: str, guild_id: int,
+                      voice_channel: VocalGuildChannel,
                       channel: Messageable, database: Database, on_search: Callable[[str], Any]):
     await on_search(f":confetti_ball: Escuchando Confetti en horas de trabajo...")
     database.register_user_interaction(author_name, "confetti")
-    yt_dlp_info = extract_yt_dlp_info("https://www.youtube.com/channel/UCyFr9xzU_lw9cDA69T0EmGg")
+    yt_dlp_info = extract_yt_dlp_info(
+        url="https://music.youtube.com/playlist?list=OLAK5uy_nX7fhyH24rCKS9pZ2baLnIIwc-q5yyCcY",
+        retrieve_full_playlist=True
+    )
     if yt_dlp_info is not None:
-        songs = yt_dlp_info.get('entries')
-        if number_of_songs < len(songs):
-            songs = random.sample(yt_dlp_info.get('entries'), number_of_songs)
-        songs = list(map(lambda song: song.get('url'), songs))
+        entries = yt_dlp_info.get('entries')
+        real_confetti_entries = [entry for entry in entries if entry.get('id') not in NOT_CONFETTI_SONGS]
+        picked_confetti_entries = random.sample(real_confetti_entries, min(number_of_songs, len(real_confetti_entries)))
+        song_urls = list(map(lambda entry: entry.get('url'), picked_confetti_entries))
         await on_play(
-            sounds=songs,
+            sounds=song_urls,
             author_name=author_name,
             guild_id=guild_id,
             database=database,
